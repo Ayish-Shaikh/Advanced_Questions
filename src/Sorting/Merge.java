fd5df4386a99ffd06f3480798801d778aa846c6a
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2025-12-27 19:01:58 +0530

    merge sort commited

---
+++ src/Sorting/Merge.java
@@ -0,0 +1,12 @@
+package Sorting;
+
+public class Merge {
+    public static void MergeSort(int[] arr){
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        int mid = (low + high) / 2;
+
+
+    }
+}
