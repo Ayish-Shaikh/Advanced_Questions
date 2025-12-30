e7f2e46c101a4c2b147fc31089feba7cae67571c
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2025-12-30 22:31:40 +0530

    traverse a linked list updated

---
+++ src/LinkedList/TraversalInSLL.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+public class TraversalInSLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public static void traverse(Node head) {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.print("null");
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(2);
+        head.next = new Node(5);
+        head.next.next = new Node(7);
+        head.next.next.next = new Node(11);
+        head.next.next.next.next = new Node(1);
+        head.next.next.next.next.next = new Node(3);
+
+        traverse(head);
+    }
+}
