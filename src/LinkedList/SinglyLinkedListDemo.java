8335df7d1438986d2023df44787c3bc91fc226f5
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2025-12-30 22:16:54 +0530

    array to linked list

--- src/LinkedList/SinglyLinkedListDemo.java
+++ src/LinkedList/SinglyLinkedListDemo.java
@@ -11,9 +11,25 @@ public class SinglyLinkedListDemo {
         }
     }
 
+    private static Node convertarr2ll(int[] arr1){
+        Node head = new Node(arr1[0]);
+        Node mover = head;
+
+        for (int i = 0; i < arr1.length; i++) {
+            Node temp = new Node(arr1[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
     public static void main(String[] args) {
         int[] arr = {2, 4, 3, 9, 16, 1, 17};
         Node node = new Node(arr[3]);
         System.out.println(node.data);
+
+        //array to linkedlist
+        int[] arr1 = {2, 4, 3, 9, 16, 1, 17};
+        Node head = convertarr2ll(arr1);
+        System.out.println(head.data);
     }
 }
