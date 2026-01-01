0360a359590abdbb583faada3b1cf7bdc3ecb6be
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-01-01 21:12:55 +0530

    insertion of value at any point is commited

---
+++ src/LinkedList/SinglyLinkedListRevision/InsertionOfValueInSLL.java
@@ -0,0 +1,62 @@
+package LinkedList.SinglyLinkedListRevision;
+
+public class InsertionOfValueInSLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public static void traverse(Node head) {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static Node InsertBeforeValue(Node head, int element, int k){
+        if (head == null) {
+            return head;
+        }
+        if (head.data == k) {
+            Node newNode = new Node(element);
+            newNode.next = head;
+            return newNode;
+        }
+        Node temp = head;
+
+        while (temp.next != null) {
+            if (temp.next.data == k){
+                Node node = new Node(element);
+                node.next = temp.next;
+                temp.next = node;
+                return head;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(2);
+        head.next = new Node(5);
+        head.next.next = new Node(7);
+        head.next.next.next = new Node(11);
+        head.next.next.next.next = new Node(1);
+        head.next.next.next.next.next = new Node(3);
+
+        System.out.println("Before Inserting head at any point of the node: ");
+        traverse(head);
+
+        System.out.println("After Inserting head at any point of the node: ");
+        head = InsertBeforeValue(head, 100, 7);
+        traverse(head);
+    }
+}
