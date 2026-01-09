9f7ed91345c8d121103c0164458c216249db97d2
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-01-09 21:01:40 +0530

    selection sort

---
+++ src/LinkedList/RevisionALL/ssort.java
@@ -0,0 +1,30 @@
+package LinkedList.RevisionALL;
+
+public class ssort {
+    public static void selection(int[] arr){
+        int n = arr.length;
+
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64 , 25 , 12 , 22 , 11};
+
+        selection(arr);
+
+        System.out.println("Sorted Array : ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
