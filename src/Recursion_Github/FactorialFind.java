a63ab1cd8369472b545464a5212f448f21a60925
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-01-14 19:49:47 +0530

    updated factorial find

--- src/Recursion_Github/FactorialFind.java
+++ src/Recursion_Github/FactorialFind.java
@@ -2,12 +2,13 @@ package Recursion_Github;
 
 public class FactorialFind {
     public static void main(String[] args) {
-        System.out.println(factorialFind(5));
+        System.out.println("finding the factorial of a given number: " + factorialFind(5));
     }
         public static int factorialFind ( int n){
             if (n == 1) {
                 return 1;
             }
+            System.out.println(n);
             return n * factorialFind(n - 1);
         }
     }
