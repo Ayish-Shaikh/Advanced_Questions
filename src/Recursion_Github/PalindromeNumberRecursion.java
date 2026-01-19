00d73179c823b2e7d2da2ec8d8bc637e81dcedef
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-01-19 19:27:59 +0530

    palindrome number done with recursion

---
+++ src/Recursion_Github/PalindromeNumberRecursion.java
@@ -0,0 +1,21 @@
+package Recursion_Github;
+
+public class PalindromeNumberRecursion {
+    public static void main(String[] args) {
+        String test = "kppappk";
+        boolean result = printNo(test, 0, test.length()-1);
+        if (result) {
+            System.out.println("String: " + test);
+            System.out.println("it is a palindrome");
+        }else {
+            System.out.println("String: " + test);
+            System.out.println("not a palindrome");
+        }
+    }
+    public static boolean printNo(String str, int i, int j){
+        if (i >= j) {
+            return true;
+        }
+        return (str.charAt(i) == str.charAt(j) && printNo(str, i+1, j-1));
+    }
+}
