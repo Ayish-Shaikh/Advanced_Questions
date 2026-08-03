4909521bbbedc41b8ebd9b50a3f97c059338b3c1
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-08-03 21:45:52 +0530

    add: reverse a word using stack

--- src/stack/reverse_word_in_sentence.java
+++ src/stack/reverse_word_in_sentence.java
@@ -1,4 +1,23 @@
 package stack;
 
 public class reverse_word_in_sentence {
+    public static void main(String[] args){
+        String str = "I Love Java";
+        Stack<String> stack = new Stack<>();
+
+        String[] words = str.trim().split(" ");
+
+        for ( int i = 0; i < words.length; i++){
+            stack.push(words[i]);
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            result.append(stack.pop( )).append(" ");
+        }
+
+        System.out.println("Original: " + str);
+        System.out.println("Reversed: " + result);
+    }
 }
